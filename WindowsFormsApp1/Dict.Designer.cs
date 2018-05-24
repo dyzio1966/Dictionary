70d05782cad82559ad60a0d50ef41a12bf40d6f2
dyzio1966 <r4p5w7@o2.pl>
2018-05-24 23:56:52 +0200

    Initial/test version.

---
+++ WindowsFormsApp1/Dict.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CollinsDict
+{
+    partial class Dict
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dict));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbWords = new System.Windows.Forms.ListBox();
+            this.txtEntry = new System.Windows.Forms.RichTextBox();
+            this.txInput = new System.Windows.Forms.TextBox();
+            this.cbDirection = new System.Windows.Forms.ComboBox();
+            this.pbFlag = new System.Windows.Forms.PictureBox();
+            this.ilFlags = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(575, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbWords
+            // 
+            this.lbWords.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbWords.DisplayMember = "text";
+            this.lbWords.FormattingEnabled = true;
+            this.lbWords.Location = new System.Drawing.Point(13, 67);
+            this.lbWords.Name = "lbWords";
+            this.lbWords.ScrollAlwaysVisible = true;
+            this.lbWords.Size = new System.Drawing.Size(187, 251);
+            this.lbWords.TabIndex = 2;
+            this.lbWords.ValueMember = "id";
+            this.lbWords.SelectedIndexChanged += new System.EventHandler(this.lbWords_SelectedIndexChanged);
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEntry.AutoWordSelection = true;
+            this.txtEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtEntry.HideSelection = false;
+            this.txtEntry.Location = new System.Drawing.Point(206, 41);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(444, 277);
+            this.txtEntry.TabIndex = 3;
+            this.txtEntry.Text = "";
+            this.txtEntry.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtEntry_MouseDoubleClick);
+            // 
+            // txInput
+            // 
+            this.txInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txInput.Location = new System.Drawing.Point(13, 41);
+            this.txInput.Name = "txInput";
+            this.txInput.Size = new System.Drawing.Size(187, 20);
+            this.txInput.TabIndex = 1;
+            this.txInput.TextChanged += new System.EventHandler(this.txInput_TextChanged);
+            // 
+            // cbDirection
+            // 
+            this.cbDirection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDirection.FormattingEnabled = true;
+            this.cbDirection.Items.AddRange(new object[] {
+            "English To Polish dictionary",
+            "Polish To English dictionary"});
+            this.cbDirection.Location = new System.Drawing.Point(50, 12);
+            this.cbDirection.Name = "cbDirection";
+            this.cbDirection.Size = new System.Drawing.Size(289, 21);
+            this.cbDirection.TabIndex = 4;
+            this.cbDirection.SelectedIndexChanged += new System.EventHandler(this.cbDirection_SelectedIndexChanged);
+            // 
+            // pbFlag
+            // 
+            this.pbFlag.Location = new System.Drawing.Point(13, 5);
+            this.pbFlag.Name = "pbFlag";
+            this.pbFlag.Size = new System.Drawing.Size(32, 32);
+            this.pbFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFlag.TabIndex = 6;
+            this.pbFlag.TabStop = false;
+            this.pbFlag.Click += new System.EventHandler(this.pbFlag_Click);
+            // 
+            // ilFlags
+            // 
+            this.ilFlags.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilFlags.ImageStream")));
+            this.ilFlags.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilFlags.Images.SetKeyName(0, "if_Flag_of_United_Kingdom_96354.png");
+            this.ilFlags.Images.SetKeyName(1, "if_Flag_of_Poland_96372.png");
+            // 
+            // Dict
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 326);
+            this.Controls.Add(this.pbFlag);
+            this.Controls.Add(this.cbDirection);
+            this.Controls.Add(this.txInput);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.lbWords);
+            this.Controls.Add(this.btnExit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Dict";
+            this.Text = "Dict";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Dict_FormClosing);
+            this.Load += new System.EventHandler(this.Dict_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Dict_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lbWords;
+        private System.Windows.Forms.RichTextBox txtEntry;
+        private System.Windows.Forms.TextBox txInput;
+        private System.Windows.Forms.ComboBox cbDirection;
+        private System.Windows.Forms.PictureBox pbFlag;
+        private System.Windows.Forms.ImageList ilFlags;
+    }
+}
+
---
+++ WindowsFormsApp1/Dict.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Globalization;
+using CollinsDict.Properties;
+using System.Text.RegularExpressions;
+
+namespace CollinsDict
+{
+    public partial class Dict : Form
+    {
+        public class Word
+        {
+            public string text { get; set; }
+            public long id { get; set; }
+            public Word(string aText, long aId)
+            {
+                this.text = aText;
+                this.id = aId;
+            }
+
+            public override string ToString() { return this.text; }
+        }
+
+        // Holds our connection with the database
+        SQLiteConnection m_dbConnection;
+        List<Word> m_words;
+        bool m_isPl = false;
+        bool m_changing = false;
+        bool m_isInSearch = false;
+        bool m_isInSelect = false;
+        string m_newWord = "";
+
+        public Dict()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fillWordsList()
+        {
+            string sql = "select * from word{0} order by id";
+            sql = String.Format(sql, m_isPl ? "_pl" : "");
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            m_words = new List<Word>();
+            while (reader.Read())
+            {
+                m_words.Add(new Word((string)reader["text"], (long)reader["id"]));
+            }
+            lbWords.DataSource = m_words;
+            lbWords.DisplayMember = "text";
+            lbWords.ValueMember = "id";
+        }
+
+        private void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=dictsqlite3.db;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        private void fillEntry(long num)
+        {
+            string sql = "select * from entry{0} where word_id=" + num.ToString();
+            sql = String.Format(sql, m_isPl ? "_pl" : "");
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            txtEntry.ResetText();
+
+            if (reader.Read())
+            {
+                string s = @"{\rtf {\colortbl;\red128\green0\blue0;\red76\green23\blue139;}" + (string)reader["text_rtf"] + "}";
+                txtEntry.Rtf = s;
+            }
+        }
+
+        private void Dict_Load(object sender, EventArgs e)
+        {
+            initAll();
+        }
+
+        private void initAll()
+        {
+            initPos();
+            connectToDatabase();
+            initState();
+        }
+
+        private void initPos()
+        {
+            // Set window location
+            if (Settings.Default.WindowLocation != null)
+            {
+                Point savedLoc = Settings.Default.WindowLocation;
+                if (IsOnScreen(savedLoc))
+                {
+                    this.Location = savedLoc;
+                }
+            }
+
+            // Set window size
+            if (Settings.Default.WindowSize != null)
+            {
+                this.Size = Settings.Default.WindowSize;
+            }
+        }
+
+        private bool IsOnScreen(Point formTopLeft)
+        {
+            Screen[] screens = Screen.AllScreens;
+            foreach (Screen screen in screens)
+            {
+                if (screen.WorkingArea.Contains(formTopLeft))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void initState()
+        {
+            fillWordsList();
+            cbDirection.SelectedIndex = m_isPl ? 1 : 0;
+            pbFlag.Image = ilFlags.Images[m_isPl ? 1 : 0];
+            txtEntry.Text = "";
+            txtEntry.Rtf = "";
+            txtEntry.ResetText();
+            txInput.Text = m_newWord;
+            m_newWord = "";
+            txInput.Select();
+        }
+
+        private void lbWords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            m_isInSelect = true;
+            fillEntry((long)lbWords.SelectedValue);
+            if (!m_isInSearch)
+            {
+                txInput.Text = lbWords.Text;
+            }
+            m_isInSelect = false;
+        }
+
+        private void txInput_TextChanged(object sender, EventArgs e)
+        {
+            if (m_isInSelect) return;
+            m_isInSearch = true;
+            MatchWord(txInput.Text);
+            m_isInSearch = false;
+        }
+
+        private void IncrementalSearch(char ch)
+        {
+            //if ((DateTime.Now - this.lastKeyPressTime) > new TimeSpan(0, 0, 1))
+            //{
+            //    this.searchString = ch.ToString();
+            //}
+            //else
+            //{
+            //    this.searchString += ch;
+            //}
+            //this.lastKeyPressTime = DateTime.Now;
+            //* code falls over HERE *//
+            string curVal = txInput.Text + ch;
+
+            m_isInSearch = true;
+            MatchWord(curVal);
+            m_isInSearch = false;
+        }
+
+        private void MatchWord(string curVal)
+        {
+            var item =
+                lbWords.Items.Cast<Word>()
+                    .FirstOrDefault(it => it.text.StartsWith(curVal, true, CultureInfo.InvariantCulture));
+
+            if (item == null)
+            {
+                MatchWord(curVal.Remove(curVal.Length - 1));
+                return;
+            }
+            var index = lbWords.Items.IndexOf(item);
+            if (index < 0) return;
+            lbWords.SelectedIndex = index;
+        }
+
+        private void cbDirection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setDict(cbDirection.SelectedIndex == 1);
+        }
+
+        private void setDict(bool isPl)
+        {
+            if (m_changing) return;
+            m_changing = true;
+            m_isPl = isPl;
+            initState();
+            m_changing = false;
+        }
+
+        private void setPl()
+        {
+            if (m_isPl) return;
+            m_isPl = true;
+            initState();
+        }
+
+        private void setEng()
+        {
+            if (!m_isPl) return;
+            m_isPl = false;
+            initState();
+        }
+
+        private void Dict_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F1:
+                    setEng();
+                    e.Handled = true;
+                    break;
+                case Keys.F2:
+                    setPl();
+                    e.Handled = true;
+                    break;
+                case Keys.C:
+                    if (e.Control && !e.Alt)
+                    {
+                        if (this.ActiveControl.Name == "txtEntry")
+                        {
+                            Clipboard.SetText(txtEntry.SelectedText);
+                            e.Handled = true;
+                        }
+                        else if (this.ActiveControl.Name == "txInput")
+                        {
+                            Clipboard.SetText(txInput.SelectedText);
+                            e.Handled = true;
+                        }
+                        else if (this.ActiveControl.Name == "lbWords")
+                        {
+                            Clipboard.SetText(lbWords.Text);
+                            e.Handled = true;
+                        }
+                        else
+                        {
+                            e.Handled = false;
+                        }
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                    break;
+                case Keys.Escape:
+                    txInput.Text = "";
+                    txInput.Select();
+                    break;
+                default:
+                    e.Handled = false;
+                    break;
+            }
+        }
+
+        private void Dict_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Copy window location to app settings
+            Point currLoc = this.Location;
+            if (IsOnScreen(currLoc))
+            {
+                Settings.Default.WindowLocation = currLoc;
+            }
+
+            // Copy window size to app settings
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Settings.Default.WindowSize = this.Size;
+            }
+            else
+            {
+                Settings.Default.WindowSize = this.RestoreBounds.Size;
+            }
+
+            // Save settings
+            Settings.Default.Save();
+        }
+
+        private void txtEntry_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string selRtf = txtEntry.SelectedRtf;
+            if (Regex.Replace(selRtf, @"{.*}", String.Empty).Split(' ')[0].Contains("\\cf1"))
+            {
+                m_newWord = txtEntry.SelectedText.Trim();
+                // it means this is explanation - language has to be changed
+                setDict(isPl: !m_isPl);
+                //Application.DoEvents();
+                //System.Threading.Thread.Sleep(100);
+            }
+            else
+            {
+                txInput.Text = txtEntry.SelectedText.Trim();
+            }
+        }
+
+        private void pbFlag_Click(object sender, EventArgs e)
+        {
+            setDict(isPl: !m_isPl);
+        }
+    }
+}
